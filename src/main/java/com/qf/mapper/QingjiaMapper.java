06f5a54f0a92a5c3165445086e364fb0a7341636
ZangYuY <1455979241@qq.com>
2019-11-09 14:29:49 +0800

    添加

--- src/main/java/com/qf/mapper/QingjiaMapper.java
+++ src/main/java/com/qf/mapper/QingjiaMapper.java
@@ -2,6 +2,8 @@ package com.qf.mapper;
 
 import com.qf.pojo.Holiday;
 
+import java.util.List;
+
 public interface QingjiaMapper {
 
     //增加请假---学生发起请假
@@ -9,4 +11,12 @@ public interface QingjiaMapper {
 
     //修改请假的状态--老师审批后
     public int updQingjiaStatus(int hid);
+
+    /**
+     * 查询待审批的假条列表
+     * 参数list表示待审批的假条id集合
+     * @param list
+     * @return
+     */
+    public List<Holiday> getApproveHolidayList(List<String> list);
 }
